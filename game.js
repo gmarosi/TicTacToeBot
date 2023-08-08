be22630b2f11781abcbe06ee825d088565c51e39
Marosi Gergely <gmarosi03@gmail.com>
2023-08-08 14:16:40 +0200

    check for own and opponent near wins

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 let xTurn = true;
-const xPositions = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-const oPositions = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+const xPositions = [false, false, false, false, false, false, false, false, false];
+const oPositions = [false, false, false, false, false, false, false, false, false];
 let gameOver = false;
+let moveCount = 0;
 const wins = [0, 0] // x, o
 
 function move(id)
@@ -21,34 +22,37 @@ function move(id)
     if(xTurn)
     {
         document.getElementById(id).innerHTML = 'X';
-        xPositions[id] = 1;
+        xPositions[id] = true;
         xTurn = false;
     }
     else
     {
         document.getElementById(id).innerHTML = 'O';
-        oPositions[id] = 1;
+        oPositions[id] = true;
         xTurn = true;
     }
 
+    moveCount++;
     checkWin();
+    if(!gameOver && !xTurn)
+        think(id);
 }
 
 function checkWin()
 {
-    let mainDiag = xPositions[0] + xPositions[4] + xPositions[8] == 3 || oPositions[0] + oPositions[4] + oPositions[8] == 3;
-    let sideDiag = xPositions[2] + xPositions[4] + xPositions[6] == 3 || oPositions[2] + oPositions[4] + oPositions[6] == 3;
+    let mainDiag = xPositions[0] && xPositions[4] && xPositions[8] || oPositions[0] && oPositions[4] && oPositions[8];
+    let sideDiag = xPositions[2] && xPositions[4] && xPositions[6] || oPositions[2] && oPositions[4] && oPositions[6];
     const columns = [false, false, false];
     const rows = [false, false, false];
 
     for(i = 0; i < 3; i++)
     {
-        columns[i] = xPositions[i] + xPositions[i + 3] + xPositions[i + 6] == 3 || oPositions[i] + oPositions[i + 3] + oPositions[i + 6] == 3;
+        columns[i] = xPositions[i] && xPositions[i + 3] && xPositions[i + 6] || oPositions[i] && oPositions[i + 3] && oPositions[i + 6];
     }
 
     for(i = 0; i < 7; i += 3)
     {
-        rows[i / 3] = xPositions[i] + xPositions[i + 1] + xPositions[i + 2] == 3 || oPositions[i] + oPositions[i + 1] + oPositions[i + 2] == 3
+        rows[i / 3] = xPositions[i] && xPositions[i + 1] && xPositions[i + 2] || oPositions[i] && oPositions[i + 1] && oPositions[i + 2];
     }
 
     if(mainDiag || sideDiag || columns[1] || rows[1])
@@ -69,6 +73,188 @@ function checkWin()
         gameOver = true;
         scoring(document.getElementById("8").innerHTML);
     }
+}
+
+function think(id)
+{
+    /*
+    Near wins:
+    first row:
+    0,1; 0,2; 1,2
+
+    first column:
+    0,3; 0,6; 3,6
+
+    main diagonal:
+    0,4; 0,8; 4,8
+
+    side diagonal:
+    2,4; 2,6; 4,6
+
+    second row:
+    3,4; 3,5; 4,5
+
+    second column:
+    1,4; 1,7; 4,7
+
+    third row:
+    6,7; 6,8; 7,8
+
+    third column:
+    2,5; 2,8; 5,8
+    */
+
+    // Check for own near wins
+
+    let checked = false;
+    let o = 0;
+    const emptyFields = new Array();
+    let i = 0;
+    while(!checked)
+    {
+        while(i < 9 && !oPositions[i])
+        {
+            i++;
+        }
+        if(i >= 9)
+        {
+            checked = true;
+            break;
+        }
+
+        for(num of rowOf(i))
+        {
+            if(oPositions[num])
+            {
+                o++;
+            }
+            else if(xPositions[num])
+            {
+                o = 0;
+                break;
+            }
+            else
+            {
+                emptyFields.push(num);
+            }
+        }
+
+        if(o == 2)
+        {
+            move(emptyFields[0]);
+            return;
+        }
+
+        o = 0;
+        while(emptyFields.length > 0)
+        {
+            emptyFields.pop();
+        }
+        for(num of columnOf(i))
+        {
+            if(oPositions[num])
+            {
+                o++;
+            }
+            else if(xPositions[num])
+            {
+                o = 0;
+                break;
+            }
+            else
+            {
+                emptyFields.push(num);
+            }
+        }
+
+        if(o == 2)
+        {
+            move(emptyFields[0]);
+            return;
+        }
+
+        //TODO: check diag
+
+        i++;
+    }
+
+    // Check if prev move caused near win
+    let x = 0;
+    while(emptyFields.length > 0)
+    {
+        emptyFields.pop();
+    }
+    for(num of rowOf(id))
+    {
+        if(xPositions[num])
+        {
+            x++;
+        }
+        else if(oPositions[num])
+        {
+            x = 0;
+            break;
+        }
+        else
+        {
+            emptyFields.push(num);
+        }
+    }
+
+    if(x == 2)
+    {
+        move(emptyFields[0]);
+        return;
+    }
+
+    x = 0;
+    while(emptyFields.length > 0)
+    {
+        emptyFields.pop();
+    }
+    for(num of columnOf(id))
+    {
+        if(xPositions[num])
+        {
+            x++;
+        }
+        else if(oPositions[num])
+        {
+            x = 0;
+            break;
+        }
+        else
+        {
+            emptyFields.push(num);
+        }
+    }
+
+    if(x == 2)
+    {
+        move(emptyFields[0]);
+        return;
+    }
+
+    //TODO: check diag
+
+    // If possible, place center
+    if(!xPositions[4] && !oPositions[4])
+    {
+        move(4);
+        return;
+    }
+}
+
+function rowOf(num)
+{
+    let rowStart = Math.floor(num/3) * 3;
+    return [rowStart, rowStart + 1, rowStart + 2];
+}
+
+function columnOf(num)
+{
+    let colStart = num % 3;
+    return [colStart, colStart + 3, colStart + 6];
 }
 
 function scoring(winner)
@@ -95,6 +281,7 @@ function restart()
     }
     xTurn = true;
     gameOver = false;
+    moveCount = 0;
 }
 
 function reset()
